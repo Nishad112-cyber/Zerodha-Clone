a2c75b62ed5f8b907c70e74883d4b2030d821774
Nishad112-cyber <nishadguptad@gmail.com>
2026-03-03 22:39:35 +0530

    landing page style

--- frontend/src/landing-page/Home/Award.jsx
+++ frontend/src/landing-page/Home/Award.jsx
@@ -1,7 +1,49 @@
 import React from 'react';
 function Award() {
     return (  
-        <h1>Award</h1>
+        <div className='container'>
+            <div className='row'>
+                <div className='col-6 mt-5'>
+                <img src="images/largestBroker.svg" 
+                 style={{width:"90%"}}
+                alt="Awarded" />
+                </div>
+                <div className='col-6 mt-5 '>
+                    <h2>Largest stack broker in india</h2>
+                    <p>2+ million Zerodha clients contribute to over 15% of all retail order volumes in India daily by trading and investing in:</p>
+                    
+                        <div className='row '>
+                        <div className='col-6'>
+                            <ul>
+                        <li>
+                            <p>Futures and Options</p>
+                        </li>
+                        <li>
+                            <p>Commodity derivatives</p>
+                        </li>
+                        <li>
+                            <p>Currency derivatives</p>
+                        </li>
+                        </ul>
+                         </div>
+                         <div className='col-6'>
+                            <ul>
+                        <li>
+                            <p>Stocks & IPOs</p>
+                        </li>
+                        <li>
+                            <p>Direct mutual funds</p>
+                        </li>
+                        <li>
+                            <p>Bonds and Government</p>
+                        </li>
+                    </ul>
+                </div>
+                </div>
+                <img src="images/pressLogos.png" style={{width:"90%"}}alt="press" />
+                </div>
+            </div>
+        </div>
     );
 }
 
