8adcb5bad2a31710534d49cd4318b8b13d703cb6
Nishad112-cyber <nishadguptad@gmail.com>
2026-03-05 23:10:26 +0530

    added new

---
+++ frontend/src/landing-page/Support/RiaseTicket.jsx
@@ -0,0 +1,146 @@
+import React from "react";
+function RiaseTicket(){
+    return(
+         <div className="container">
+      <div className="row p-5 mt-5 mb-5">
+        <h1 className="fs-3">
+          To create a ticket , select a relavent topic
+        </h1>
+        <div className="col-4  mt-4">
+            <h4 className=""><i class="fa-solid fa-square-plus"></i>Account Openning
+            </h4>
+    
+
+              <a href=""style={{lineHeight:"2.5"}}>Open demat account</a>
+              <br></br>
+              <a href=""style={{lineHeight:"2.5"}}>Minor demat account</a>
+              <br></br>
+              <a href=""style={{lineHeight:"2.5"}}>NRI demat account</a>
+              <br></br>
+              <a href=""style={{lineHeight:"2.5"}}>Commodity</a>
+              <br></br>
+              <a href=""style={{lineHeight:"2.5"}}>Dematerialisation</a>
+              <br></br>
+              <a href=""style={{lineHeight:"2.5"}}>Fund transfer</a>
+              <br></br>
+              <a href="">MTF</a>
+              <br></br>
+              <a href="">Referral program</a>
+
+        </div>
+         <div className="col-4  mt-4">
+            <h4 className=""><i class="fa-solid fa-square-plus"></i>Account Openning
+            </h4>
+    
+
+              <a href=""style={{lineHeight:"2.5"}}>Open demat account</a>
+              <br></br>
+              <a href=""style={{lineHeight:"2.5"}}>Minor demat account</a>
+              <br></br>
+              <a href=""style={{lineHeight:"2.5"}}>NRI demat account</a>
+              <br></br>
+              <a href=""style={{lineHeight:"2.5"}}>Commodity</a>
+              <br></br>
+              <a href=""style={{lineHeight:"2.5"}}>Dematerialisation</a>
+              <br></br>
+              <a href=""style={{lineHeight:"2.5"}}>Fund transfer</a>
+              <br></br>
+              <a href="">MTF</a>
+              <br></br>
+              <a href="">Referral program</a>
+
+        </div>
+         <div className="col-4  mt-4">
+            <h4 className=""><i class="fa-solid fa-square-plus"></i>Account Openning
+            </h4>
+    
+
+              <a href=""style={{lineHeight:"2.5"}}>Open demat account</a>
+              <br></br>
+              <a href=""style={{lineHeight:"2.5"}}>Minor demat account</a>
+              <br></br>
+              <a href=""style={{lineHeight:"2.5"}}>NRI demat account</a>
+              <br></br>
+              <a href=""style={{lineHeight:"2.5"}}>Commodity</a>
+              <br></br>
+              <a href=""style={{lineHeight:"2.5"}}>Dematerialisation</a>
+              <br></br>
+              <a href=""style={{lineHeight:"2.5"}}>Fund transfer</a>
+              <br></br>
+              <a href="">MTF</a>
+              <br></br>
+              <a href="">Referral program</a>
+
+        </div>
+         <div className="col-4  mt-4">
+            <h4 className=""><i class="fa-solid fa-square-plus"></i>Account Openning
+            </h4>
+    
+
+              <a href=""style={{lineHeight:"2.5"}}>Open demat account</a>
+              <br></br>
+              <a href=""style={{lineHeight:"2.5"}}>Minor demat account</a>
+              <br></br>
+              <a href=""style={{lineHeight:"2.5"}}>NRI demat account</a>
+              <br></br>
+              <a href=""style={{lineHeight:"2.5"}}>Commodity</a>
+              <br></br>
+              <a href=""style={{lineHeight:"2.5"}}>Dematerialisation</a>
+              <br></br>
+              <a href=""style={{lineHeight:"2.5"}}>Fund transfer</a>
+              <br></br>
+              <a href="">MTF</a>
+              <br></br>
+              <a href="">Referral program</a>
+
+        </div>
+         <div className="col-4  mt-4">
+            <h4 className=""><i class="fa-solid fa-square-plus"></i>Account Openning
+            </h4>
+    
+
+              <a href=""style={{lineHeight:"2.5"}}>Open demat account</a>
+              <br></br>
+              <a href=""style={{lineHeight:"2.5"}}>Minor demat account</a>
+              <br></br>
+              <a href=""style={{lineHeight:"2.5"}}>NRI demat account</a>
+              <br></br>
+              <a href=""style={{lineHeight:"2.5"}}>Commodity</a>
+              <br></br>
+              <a href=""style={{lineHeight:"2.5"}}>Dematerialisation</a>
+              <br></br>
+              <a href=""style={{lineHeight:"2.5"}}>Fund transfer</a>
+              <br></br>
+              <a href="">MTF</a>
+              <br></br>
+              <a href="">Referral program</a>
+
+        </div>
+         <div className="col-4  mt-4">
+            <h4 className=""><i class="fa-solid fa-square-plus"></i>Account Openning
+            </h4>
+    
+
+              <a href=""style={{lineHeight:"2.5"}}>Open demat account</a>
+              <br></br>
+              <a href=""style={{lineHeight:"2.5"}}>Minor demat account</a>
+              <br></br>
+              <a href=""style={{lineHeight:"2.5"}}>NRI demat account</a>
+              <br></br>
+              <a href=""style={{lineHeight:"2.5"}}>Commodity</a>
+              <br></br>
+              <a href=""style={{lineHeight:"2.5"}}>Dematerialisation</a>
+              <br></br>
+              <a href=""style={{lineHeight:"2.5"}}>Fund transfer</a>
+              <br></br>
+              <a href="">MTF</a>
+              <br></br>
+              <a href="">Referral program</a>
+
+        </div>
+      </div>
+      </div>
+    );
+}
+
+export default RiaseTicket;
